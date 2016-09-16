4573eef7c61083defad120fbc9bd2a2913220746
Idar Syslak <idasys1@outlook.com>
2016-09-16 10:44:00 +0200

    venter med parseMember til vi eventuelt har en Member-klasse

--- WebContent/script/ajaxcomm.js
+++ WebContent/script/ajaxcomm.js
@@ -47,32 +47,64 @@ class AjaxComm {
 	
 	gotUpdate(responseText) {
 		console.log("AjaxComm: Recieved data \"" + receivedData + "\".")
-		let jsonObject = JSON.parse(responseText)
+		let jsonObject = JSON.parse(responseText).updates
 		
 		// Proceed if request was understood and processed
-		if (jsonObject.updates.status) {
-			this.logId = jsonObject.updates.logId
+		if (jsonObject.status) {
+			this.logId = jsonObject.logId
 			
 			// Check if there are new members since last update
-			if (jsonObject.updates.hasOwnProperty('newMembers')) {
+			if (jsonObject.hasOwnProperty('newMembers')) {
 				// Check if there are multiple new members
-				if (jsonObject.updates.newMembers instanceof Array) {
+				if (jsonObject.newMembers instanceof Array) {
 					// Multiple new entries; iterate
+					for (let i of jsonObject.newMembers) {
+						member = parseMember(i)
+						// UIHandler.addRow(member)
+					}
 				} else {
 					// Single new entry
+					member = parseMember(jsonObject.newMembers)
+					// UIHandler.addRow(member)
 				}
 			}
 			
-			// TODO iterate through updated and deleted members
+			if (jsonObject.hasOwnProperty('updatedMembers')) {
+				if (jsonObject.updatedMembers instanceof Array) {
+					for (let i of jsonObject.updatedMembers) {
+						member = parseMember(i)
+						// UIHandler.editRow(member.memberId, member)
+					}
+				} else {
+					member = parseMember(jsonObject.updatedMembers)
+					// UIHandler.editRow(member.memberId, member)
+				}
+			}
+			
+			if (jsonObject.hasOwnProperty('deletedMembers')) {
+				if (jsonObject.deletedMembers instanceof Array) {
+					for (let i of jsonObject.deletedMembers) {
+						// UIHandler.removeRow(i)
+					}
+				} else {
+					// UIHandler.removeRow(jsonObject.deletedMembers)
+				}
+			}
 		}
+	}
+	
+	// Hvis vi skal omforme json-objekt til en Member-klasse
+	parseMember(member) {
+		// TODO selve omformingen
+		return member
 	}
 	
 	gotMember(responseText) {
 		console.log("AjaxComm: Recieved data \"" + responseText + "\".")
-		let jsonObject = JSON.parse(responseText)
+		let jsonObject = JSON.parse(responseText).updatedMember
 		
 		// Send update request if member has been added/updated/deleted
-		if (jsonObject.updatedMember.status) {
+		if (jsonObject.status) {
 			getUpdate(logId);
 		}
 	}
